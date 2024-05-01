721da54030a4590de573af9298a66c4a28d154bb
Enriqué T <167785063+enriquethomsen@users.noreply.github.com>
2024-05-01 23:37:08 +0900

    Update db.js

--- backend/db/db.js
+++ backend/db/db.js
@@ -3,7 +3,7 @@ const MONGO_URL = process.env.MONGO_URL || MONGO_URL
 const db = async () => {
     try {
         mongoose.set('strictQuery', false)
-        await mongoose.connect(MONGO_URLL)
+        await mongoose.connect(MONGO_URL)
         console.log('Db Connected')
     } catch (error) {
         console.log('DB Connection Error');
