da926bdfc8239ac91588598d08fe602d534ba49e
Enriqué T <167785063+enriquethomsen@users.noreply.github.com>
2024-05-01 22:55:51 +0900

    Added Mongo Link to db.js

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
-
+const MONGO_URL = process.env.MONGO_URL || MONGO_URL
 const db = async () => {
     try {
         mongoose.set('strictQuery', false)
-        await mongoose.connect(process.env.MONGO_URL)
+        await mongoose.connect(MONGO_URLL)
         console.log('Db Connected')
     } catch (error) {
         console.log('DB Connection Error');
