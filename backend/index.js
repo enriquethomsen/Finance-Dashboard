4b24361300bf99084dbcead285c5017301479f65
Enriqué T <167785063+enriquethomsen@users.noreply.github.com>
2024-05-03 03:18:35 +0900

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,7 @@ app.get("/", (req, res) => {
 });
 
 //routes
-readdirSync('./routes').map((route) => app.use('/api/v1', require('./routes/' + route)))
+readdirSync('./routes').map((route) => app.use('/api/v1/', require('./routes/' + route)))
 
 const server = () => {
     db()
